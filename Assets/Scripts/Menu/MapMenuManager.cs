25fce7adc3e2f89a09e3b9696ba01e2bf5fde1fd
Краснов Никита <156430006+nikitakrasnov7@users.noreply.github.com>
2025-12-24 13:10:04 +0500

    finishsave

--- Assets/Scripts/Menu/MapMenuManager.cs
+++ Assets/Scripts/Menu/MapMenuManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class MapMenuManager : MonoBehaviour
@@ -8,12 +9,29 @@ public class MapMenuManager : MonoBehaviour
     [SerializeField] private Transform leftDownPoint;
     [SerializeField] private Transform rightUpPoint;
 
+    [SerializeField] TextMeshProUGUI nikname;
+    [SerializeField] TextMeshProUGUI level;
+    [SerializeField] TextMeshProUGUI missiomCount;
+
+    [SerializeField] private SessionInfoSO info;
+
+
     int maxCountMark;
     private void Start()
     {
+        Time.timeScale = 1;
         StartCoroutine(Factory());
     }
-
+    private void OnEnable()
+    {
+        GetInfo();
+    }
+    public void GetInfo()
+    {
+        nikname.text = "player " + info.Nikname;
+        level.text = "level "+info.level.ToString();
+        missiomCount.text = "mission complete " + info.missionCompleteCount.ToString();
+    }
     void CreateMark()
     {
         GameObject newM = Instantiate(newMark);
